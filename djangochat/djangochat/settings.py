277979085cef0726750b9f9c4d78e96f6e81d186
DanielDominicSavioKennedy <saviodomnic2002@gmail.com>
2023-09-27 16:14:39 +0530

    websockets established

--- djangochat/djangochat/settings.py
+++ djangochat/djangochat/settings.py
@@ -36,13 +36,14 @@ LOGOUT_URL = '/login/'
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
+    'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'channels',
     'core',
     'room',
 ]
--- djangochat/room/consumers.py
+++ djangochat/room/consumers.py
@@ -1,53 +1,49 @@
 import json
-
 from channels.generic.websocket import AsyncWebsocketConsumer
-from asgiref.sync import sync_to_async
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        print("connected")
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
-        
+
         await self.channel_layer.group_add(
-            self.room_name,
             self.room_group_name,
+            self.channel_name
         )
-        
+
         await self.accept()
         
     async def disconnect(self, close_code):
-        print("disconnected")
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
-        
+
     async def receive(self, text_data):
-        print("recieved")
+        print("received")
         data = json.loads(text_data)
         message = data['message']
         username = data['username']
         room = data['room']
-        print(data)
+        # Send the message to the group
         await self.channel_layer.group_send(
             self.room_group_name,
             {
-                'ty pe':'chat_message',
-                'message':message,
-                'username':username,
-                'room':room,
-            },
+                'type': 'chat.message',
+                'message': message,
+                'username': username,
+                'room': room
+            }
         )
-        
-    async def chat_message(self,event):
-        print("sent")
+
+    async def chat_message(self, event):
         message = event['message']
         username = event['username']
         room = event['room']
-        
+
         await self.send(text_data=json.dumps({
-                'message':message,
-                'username':username,
-                'room':room,
+            'message': message,
+            'username': username,
+            'room': room,
         }))
+        
